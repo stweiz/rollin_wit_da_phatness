9738c9774140df2fbe1c0f345ba07cf5859e2b31
Stefan Weiz <s.weiz@tarent.de>
2018-04-03 18:52:51 +0200

    Implements Login and ResetPassword vues

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <form v-on:submit="onSubmit" class="loginPage">
+    <div class="loginUsername">
+      <span>Benutzername:</span>
+      <input v-model="username">
+    </div>
+
+    <div class="loginPassword">
+      <span>Passwort:</span>
+      <input v-model="password">
+    </div>
+
+    <button v-on:click="doLogin">Login</button>
+    <button v-on:click="resetPassword">Passwort zurücksetzen</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      doLogin: function () {
+
+      },
+      resetPassword: function () {
+        this.go('resetPassword')
+      },
+      onSubmit: function () {
+        // Check data
+        // Show error or redirect after successful login
+      },
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
